374b8be411d5b57a9b8dc92e2fde7fd36bdcfeb0
FlorenciaCampos <florenciacampos460@gmail.com>
2026-02-02 15:43:08 -0300

    se agrega manejo de errores en service

--- src/services/artworks.service.js
+++ src/services/artworks.service.js
@@ -1,5 +1,18 @@
 import { getArtworkById } from "../models/artwork.model.js";
 
 export const findArtworkById = (id) => {
-  return getArtworkById(id);
+  try {
+    const artwork = getArtworkById(id);
+
+    // Error de negocio: no existe
+    if (!artwork) {
+      return null;
+    }
+
+    return artwork;
+  } catch (error) {
+    // Error técnico: algo explotó abajo (DB, bug, etc.)
+    throw new Error("Error retrieving artwork");
+  }
 };
+
