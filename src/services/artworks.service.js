4f3be81e97647aa31f162033198a7124311ee319
FlorenciaCampos <florenciacampos460@gmail.com>
2026-02-04 12:13:27 -0300

    se agrega try catch en service

--- src/services/artworks.service.js
+++ src/services/artworks.service.js
@@ -1,11 +1,28 @@
+
+
 import { getArtworkById } from "../models/artwork.model.js";
 
-export const findArtworkById = (id) => {
-  const artwork = getArtworkById(id);    
-  
-  if (!artwork) {
-    return null;
-  }
+export const findArtworkById = async (id) => {
+  try {
+    console.log(`[SERVICE] Buscando artwork con id ${id}`);
 
-  return artwork;
+    const artwork = await getArtworkById(id);
+
+    // No existe → estado válido
+    if (!artwork) {
+      console.log(`[SERVICE] Artwork id ${id} no encontrado`);
+      return null;
+    }
+
+    console.log(`[SERVICE] Artwork encontrado`);
+    return artwork;
+
+  } catch (error) {
+    // Error técnico que viene del model
+    console.error(
+      `[SERVICE ERROR] Fallo al obtener artwork id ${id}`,
+      error
+    );
+    throw error; 
+  }
 };
