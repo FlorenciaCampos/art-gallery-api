a4f7a67c171aa3e9a501149ad9765adced972304
FlorenciaCampos <florenciacampos460@gmail.com>
2026-02-17 19:02:18 -0300

    se agrega codigo en db

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
 import express from "express";
 import routes from "./routes/index.js";
+import pool from "./database/db.js";
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use("/api", routes);
+
+pool.query("SELECT NOW()")
+  .then(res => {
+    console.log("DB connected at:", res.rows[0].now);
+  })
+  .catch(err => {
+    console.error("DB connection error:", err);
+  });
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
